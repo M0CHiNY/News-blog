a7fa92cd2a4cd36675c818ff3ae98f14996e5118
Light <y.khrystiuk@dev-branch.com>
2022-07-28 13:52:49 +0300

    5117 - theming block global news

--- web/themes/custom/new_blog/src/index.js
+++ web/themes/custom/new_blog/src/index.js
@@ -4,12 +4,14 @@ scss
 
 
 import './scss/_libs.scss';
+import './scss/global-settings.scss'
 import './scss/regions/_regions.scss';
 import './scss/header/_header.scss';
 import './scss/footer/_footer.scss';
 import './scss/header/_menu.scss';
 import './scss/header/_search.scss';
 import './scss/front-page/popular-article.scss';
+import './scss/front-page/globals.scss';
 import './scss/main.scss';
 
 
