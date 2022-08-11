f7cd79d328879d97707d9bcdd63c8d8822ca3795
Light <y.khrystiuk@dev-branch.com>
2022-08-11 14:40:24 +0300

    #5254 - add API request

--- web/modules/custom/custom_weather_module/src/Plugin/Block/CustomWeatherBlock.php
+++ web/modules/custom/custom_weather_module/src/Plugin/Block/CustomWeatherBlock.php
@@ -3,6 +3,7 @@
 namespace Drupal\custom_weather_module\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use GuzzleHttp\Client;
 
 /**
  * Provides a "Custom weather module" block.
@@ -20,8 +21,31 @@ class CustomWeatherBlock extends BlockBase {
    */
   public function build() {
     return [
-      '#markup' => 'Lutsk is the city. Your IP is: ',
+      '#markup' => "{$this->getAPIweather()} district. Your IP is:  {$this->getIpUser()}",
     ];
+  }
+
+  /**
+   * Function get IP current user.
+   */
+
+  // @todo This function needed dependency.
+  public function getIpUser() {
+//    return \Drupal::request()->getClientIp();
+    return '172.21.0.6';
+  }
+
+  /**
+   * Function add settings API.
+   */
+  public function getApiWeather() {
+    $url = 'http://api.weatherapi.com/v1/current.json?key=75b555a648604b3fb3a84430221108&q=lutsk Volyn&aqi=no';
+    $client = new Client();
+    $response = $client->get($url);
+    $response_data = json_decode($response->getBody()->getContents(), TRUE);
+    $result = $response_data['location']['name'];
+    return $result;
+
   }
 
 }
